91c22dfd0dd071178dfde7e7903a07081a301c39
s7abowx-xyz <ibrahimalmleh0@gmail.com>
2026-07-06 04:50:21 +0300

    Delete router/auth directory

--- router/auth/logout.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response } from 'express';
-import { clearSessionCookie } from '../../src/auth-utils';
-
-export default async function logoutHandler(req: Request, res: Response) {
-  clearSessionCookie(res);
-  res.json({ status: true, message: 'تم تسجيل الخروج.' });
-}
